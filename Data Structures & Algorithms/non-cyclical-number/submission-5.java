bb41aaa4cac98ab78c6d419eacfa3c9f1cf57a86
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-10 20:09:41 -0400

    Add: non-cyclical-number - submission-5

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isHappy(int n) {
+
+        Set<Integer> seen = new HashSet<>();
+
+        int currentNumber = n;
+        while(!seen.contains(currentNumber) && currentNumber != 1) {
+            seen.add(currentNumber);
+            int sum = 0;
+            while(currentNumber/10 > 0) {
+                int digit = currentNumber % 10;
+                currentNumber = currentNumber / 10;
+
+                sum += digit*digit;
+            }
+            sum += currentNumber*currentNumber;
+            currentNumber = sum;
+        }
+
+        return currentNumber == 1;
+        
+    }
+}
