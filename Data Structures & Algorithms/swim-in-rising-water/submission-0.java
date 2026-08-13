fe7f894bb260d434669121b5d131451261a10f99
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-13 19:09:07 -0400

    Add: swim-in-rising-water - submission-0

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-0.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int swimInWater(int[][] grid) {
+
+        // 0 -> x, 1 -> y
+        PriorityQueue<int[]> minQueue = new PriorityQueue<>((a,b) -> grid[a[0]][a[1]] - grid[b[0]][b[1]]);
+        minQueue.add(new int[]{0,0});
+
+        return bfs(0, grid, minQueue, new boolean[grid.length][grid[0].length]);
+    }
+
+    private int bfs(int currentLevel, int[][] grid, PriorityQueue<int[]> minQueue, boolean[][] visited) {
+
+        if(visited[grid.length-1][grid[0].length-1]) {
+            return currentLevel - 1;
+        }
+
+        while(!minQueue.isEmpty() && grid[minQueue.peek()[0]][minQueue.peek()[1]] <= currentLevel) {
+            int[] coordinates = minQueue.poll();
+            visited[coordinates[0]][coordinates[1]] = true;
+
+            int[] top = new int[] {coordinates[0] - 1, coordinates[1]};
+            int[] bottom = new int[] {coordinates[0] + 1, coordinates[1]};
+            int[] left = new int[] {coordinates[0], coordinates[1] - 1};
+            int[] right = new int[] {coordinates[0], coordinates[1] + 1};
+
+            if(validCoordinate(top, grid) && !visited[top[0]][top[1]]) {
+                minQueue.add(top);
+            }
+
+            if(validCoordinate(bottom, grid) && !visited[bottom[0]][bottom[1]]) {
+                minQueue.add(bottom);
+            }
+
+            if(validCoordinate(left, grid) && !visited[left[0]][left[1]]) {
+                minQueue.add(left);
+            }
+
+            if(validCoordinate(right, grid) && !visited[right[0]][right[1]]) {
+                minQueue.add(right);
+            }
+        }
+
+        return bfs(currentLevel+1, grid, minQueue, visited);
+    }
+
+    private boolean validCoordinate(int[] coordinates, int[][] grid) {
+        return coordinates[0] >= 0 && coordinates[0] < grid.length && coordinates[1] >= 0 && coordinates[1] < grid[0].length;
+    }
+}
