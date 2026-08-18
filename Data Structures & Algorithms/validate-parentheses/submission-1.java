c302be9fd4261fd0b72039062651eb197e32c555
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-17 21:22:44 -0400

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char character : s.toCharArray()) {
+            if (!stack.isEmpty() && isOpposite(stack.peek(), character)) {
+                stack.pop();
+            } else {
+                stack.push(character);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    private boolean isOpposite(char character1, char character2) {
+        if (character1 == '(' && character2 == ')') {
+            return true;
+        } else if (character1 == '[' && character2 == ']') {
+            return true;
+        } else if (character1 == '{' && character2 == '}') {
+            return true;
+        }
+
+        return false;
+    }
+}
