0e4812b47a64bc19e2e14c1e68854e411d194deb
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-19 19:36:09 -0400

    Add: find-duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-4.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    // Time: O(n)
+    // Space: O(1)
+    public int findDuplicate(int[] nums) {
+
+        int slow = 0;
+        int fast = 0;
+
+        // Detect cycle
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+
+        int slow2 = 0;
+        while(slow != slow2) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+
+        return slow;
+    }
+}
