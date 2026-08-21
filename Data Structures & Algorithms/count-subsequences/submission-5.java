2882c4d302208ed10839a7151b770dc5ca7d43ec
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-20 22:31:21 -0400

    Add: count-subsequences - submission-5

---
+++ Data Structures & Algorithms/count-subsequences/submission-5.java
@@ -0,0 +1,22 @@
+// Time: O(mn)
+// Space: O(n)
+class Solution {
+    public int numDistinct(String s, String t) {
+        int[] dp = new int[t.length()+1];
+        int[] nextDp = new int[t.length()+1];
+
+        dp[t.length()] = 1;
+        nextDp[t.length()] = 1;
+        for(int i = s.length()-1; i >= 0; i--) {
+            for (int j = t.length()-1; j >= 0; j--) {
+                nextDp[j] = dp[j];
+                if(s.charAt(i) == t.charAt(j)) {
+                    nextDp[j] += dp[j+1]; 
+                }
+            }
+            dp = nextDp.clone();
+        }
+
+        return dp[0];
+    }
+}
