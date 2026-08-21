fca2a76a0dc126e5c40bf738fd0f4b7a2a4bd4b1
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-20 22:01:35 -0400

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.java
@@ -0,0 +1,25 @@
+// Space: O(k)
+class KthLargest {
+
+    private PriorityQueue<Integer> minQueue;
+    private int k;
+    
+    // O(nlogK)
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minQueue = new PriorityQueue<>();
+        for(int num : nums) {
+            add(num);
+        }
+    }
+    
+    // O(logK)
+    public int add(int val) {
+        minQueue.add(val);
+        if(minQueue.size() > k) {
+            minQueue.poll();
+        }
+
+        return minQueue.peek();
+    }
+}
