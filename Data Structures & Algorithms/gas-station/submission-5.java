f4537c0800eab82f2a1d1aaab09570921f89dd0b
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-21 18:20:25 -0400

    Add: gas-station - submission-5

---
+++ Data Structures & Algorithms/gas-station/submission-5.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+
+        int totalSum = 0;
+        for(int i = 0; i < gas.length; i++) {
+            totalSum += gas[i] - cost[i];
+        }
+
+        if (totalSum < 0) {
+            return -1;
+        }
+
+        int runningCost = 0;
+        int result = 0;
+        for(int i = 0; i < gas.length; i++) {
+            runningCost += gas[i] - cost[i];
+            if (runningCost < 0) {
+                runningCost = 0;
+                result = i+1;
+            }
+        }
+
+        return result;
+    }
+}
