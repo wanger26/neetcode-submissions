d6e36ab7977104cc2fc944dccb0312a77f794040
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-21 17:56:44 -0400

    Add: valid-binary-search-tree - submission-8

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-8.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// Time: O(n)
+// Space: O(h)
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long leftBoundry, long rightBoundry) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return true;
+        }
+
+        boolean leafNodeInvalidForRoot = root.left != null && root.left.val >= root.val;
+        boolean leftNodeIsNotInRange = root.left != null && (root.left.val <= leftBoundry || root.left.val >= rightBoundry);
+        if (leafNodeInvalidForRoot || leftNodeIsNotInRange) {
+            return false;
+        }
+
+        leafNodeInvalidForRoot = root.right != null && root.right.val <= root.val;
+        boolean rightNodeIsNotInRange = root.right != null && (root.right.val <= leftBoundry || root.right.val >= rightBoundry);
+        if (leafNodeInvalidForRoot || rightNodeIsNotInRange) {
+            return false;
+        }
+
+        // Current tree looks good. Check left and rigth tre
+        return isValidBST(root.left, leftBoundry, root.val) && isValidBST(root.right, root.val, rightBoundry);
+    }
+}
