cf3e3589a94105a7041eb951235913400249fb0f
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-22 11:05:04 -0400

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.java
@@ -0,0 +1,27 @@
+// Time: O(n^2*2^n)
+// Space: O(n*2^n)
+class Solution {
+
+    // Top Down
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        return new ArrayList<>(recurse(nums, new ArrayList<>(), 0));
+    }
+
+    private Set<List<Integer>> recurse(int[] nums, List<Integer> current, int index) {
+        if (index == nums.length) {
+            return Set.of(new ArrayList<>(current));
+        }
+
+        // Option 1: Include
+        int insertionIndex = current.size();
+        current.add(nums[index]);
+        Set<List<Integer>> result = new HashSet<>(recurse(nums, current, index + 1));
+        current.remove(insertionIndex);
+
+        // Option 2: Do not include
+        result.addAll(recurse(nums, current, index + 1));
+
+        return result;
+    }
+}
