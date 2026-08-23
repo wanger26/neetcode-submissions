8c76cf9aa31a4586ea203be640c26f90bfab93c2
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-23 17:15:24 -0400

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.java
@@ -0,0 +1,56 @@
+class Solution {
+
+    // Time: O(nlong)
+    // Space: O(n)
+    public int[][] merge(int[][] intervals) {
+
+        // 1  --- 3
+        // 1 ----------5
+        //                6--7
+
+        PriorityQueue<int[]> minQueue = new PriorityQueue<>((a,b) -> {
+            if(a[0] == b[0]) {
+                return a[1] - b[1];
+            }
+            return a[0] - b[0];
+        });
+
+        for(int[] interval : intervals) {
+            minQueue.add(interval);
+        }
+
+        List<int[]> result = new ArrayList<>();
+        int[] currentInterval = minQueue.poll();
+        while(!minQueue.isEmpty()) {
+            int[] nextInterval = minQueue.poll();
+
+            int currentIntervalStart = currentInterval[0];
+            int currentIntervalEnd = currentInterval[1];
+
+            int nextIntervalStart = nextInterval[0];
+            int nextIntervalEnd = nextInterval[1];
+
+            // E.g Current:   |--------| 
+            // Next:               |--------|
+            // 
+            if(nextIntervalStart <= currentIntervalEnd) {
+                currentInterval[1] = Math.max(currentIntervalEnd, nextIntervalEnd);
+            } else {
+                // E.g Current:   |--------| 
+                // Next:                     |--------|
+                result.add(currentInterval);
+                currentInterval = nextInterval;
+            }
+        }
+
+        // Add last interval
+        result.add(currentInterval);
+
+        int[][] resultArray = new int[result.size()][2];
+        for(int i=0; i < resultArray.length; i++) {
+            resultArray[i] = result.get(i);
+        }
+
+        return resultArray;
+    }
+}
