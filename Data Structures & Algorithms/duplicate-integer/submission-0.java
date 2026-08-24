bfd0cbfd4b40aed7d43ab1e4e6ab8b47e8085093
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-24 19:06:15 -0400

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        
+        Set<Integer> seen = new HashSet<>();
+        for(int num : nums) {
+            if(!seen.add(num)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
