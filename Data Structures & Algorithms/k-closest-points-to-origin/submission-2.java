6ed3287f1ee04bf92fa472c65c918696d844a5cd
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-23 20:11:59 -0400

    Add: k-closest-points-to-origin - submission-2

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-2.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    // Time: O(nlogk)
+    // Space: O(k)
+    public int[][] kClosest(int[][] points, int k) {
+
+        PriorityQueue<int[]> maxQueue = new PriorityQueue<>((a,b) -> {
+            int xA = a[0];
+            int yA = a[1];
+
+            int xB = b[0];
+            int yB = b[1];
+
+            long distanceA = xA*xA + yA*yA;
+            long distanceB = xB*xB + yB*yB;
+
+            return Long.compare(distanceB, distanceA);
+        });
+
+
+        for(int[] point : points) { // O(n)
+            maxQueue.add(point); // O(logk)
+
+            if(maxQueue.size() > k) {
+                maxQueue.poll();
+            }
+        }
+
+
+        int[][] result = new int[k][2];
+        for(int i=0; i < k; i++) {
+            result[i] = maxQueue.poll();
+        }
+
+        return result;
+    }
+}
