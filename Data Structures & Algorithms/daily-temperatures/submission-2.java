25826fb10b97f5b4c41cf6aed996f85c4aa74ef8
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-25 17:46:02 -0400

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int j = i + 1; // Start at prev answer
+
+            // While the temps at j is less than or equal to the current i, we can keep jumping
+            // until we see a bigger temperature OR we hit the end of the array
+            while (j < n && temperatures[j] <= temperatures[i]) {
+                // Special case. We already hit end of the road. No need to go further
+                if (result[j] == 0) { 
+                    j = n;
+                } else {
+                    j += result[j]; // Jump to the next higher temp
+                }
+            }
+
+            // If we did not get to the end, lets set the answer
+            if(j < n) {
+                result[i] = j-i;
+            }
+        }
+
+        return result;
+    }
+}
