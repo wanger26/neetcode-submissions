1040f414dd52c7d490a08967f2b790a5c83ed195
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-24 20:52:33 -0400

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+
+        // [0,3,5,8,10]
+        // [1,3,1,4,2]
+        // 12,3,7,1,1]
+        //1, 2,  3
+
+        // At position 0: 10
+        // At position 4: 3
+        // At position 1: 5
+        // At position 7: 3
+
+
+        // At position 1: 10-1/3 = 3
+        // At position 4: 10-4/2 = 4
+
+        TreeMap<Integer, Double> positionToTimeMap = new TreeMap<>((a, b) -> b-a);
+        for(int i=0; i < position.length; i++) {
+            double time = (double)(target - position[i])/(double)speed[i];
+            positionToTimeMap.put(position[i], time);
+        }
+
+        int fleets = 0;
+        double slowestTimeAhead = 0;
+        for(double time : positionToTimeMap.values()) {
+            if(time > slowestTimeAhead) { // Current car is slower than slowest car ahead. Will make its own fleet.
+                fleets++;
+                slowestTimeAhead = time;
+            }
+        }
+
+        return fleets;
+    }
+}
