910122ae5888d7065741c9df79906c987d9a932a
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-25 20:56:24 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    // Time: O(n!)
+    // Space: O(1)
+    public int climbStairs(int n) {
+
+        if(n < 2) {
+            return 1;
+        }
+
+        int num1 = 1;
+        int num2 = 2;
+
+        for(int i=3; i <= n; i++) {
+            int temp = num2;
+            num2 = num1 + temp;
+            num1 = temp;
+        }
+
+        return num2;
+    }
+}
