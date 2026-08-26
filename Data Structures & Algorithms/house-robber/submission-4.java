0d3879b927dd4066c9c990166502f41626a1071f
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-26 07:52:16 -0400

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    // Time: O(n)
+    // Space: O(n)
+    public int rob(int[] nums) {
+        
+        int[] dp = new int[nums.length + 2];
+        for(int i=nums.length-1; i >=0; i--) {
+            dp[i] = Math.max(nums[i] + dp[i+2], dp[i+1]);
+        }
+
+        return dp[0];
+    }
+}
