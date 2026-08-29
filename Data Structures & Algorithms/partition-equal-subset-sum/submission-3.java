a9264375ba4577c721607d96bce8f06b70b8ba31
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-28 20:54:46 -0400

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.java
@@ -0,0 +1,35 @@
+class Solution {
+    // Time: O(n*s) where s is size of subset and n is size of input
+    // SpaceL O(s) where s is size of subset and n is size of input
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int i=0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+
+        if(sum % 2 != 0) {
+            return false; // Cannot partition if sum is odd
+        }
+
+        int partitionSum = sum/2;
+
+        Set<Integer> dp = new HashSet<>();
+        dp.add(0);
+
+        for(int i=nums.length-1; i >= 0; i--) {
+            Set<Integer> newDp = new HashSet<>();
+            for(int currentSum : dp) {
+                int newSum = currentSum + nums[i];
+                if(newSum == partitionSum) {
+                    return true;
+                }
+                newDp.add(newSum);
+                newDp.add(currentSum);
+            }
+            dp = newDp;
+        }
+
+        return false;
+        
+    }
+}
