99bd698c50cbb92b82ff74cead208bd7c21b30ce
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-30 09:59:15 -0400

    Add: hand-of-straights - submission-8

---
+++ Data Structures & Algorithms/hand-of-straights/submission-8.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        Map<Integer, Integer> numToFrequency = new HashMap<>();
+        for (int num : hand) {
+            numToFrequency.put(num, numToFrequency.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : hand) {
+            int start = num;
+            while(numToFrequency.getOrDefault(start - 1, 0) > 0) {
+                start--;
+            }
+            while(start <= num) {
+                while(numToFrequency.getOrDefault(start, 0) > 0) {
+                    for(int i = start; i < start + groupSize; i++) {
+                        if(numToFrequency.getOrDefault(i, 0) == 0) {
+                            return false;
+                        }
+                        numToFrequency.put(i, numToFrequency.get(i) - 1);
+                    }
+                }
+                start++;
+            }
+        }
+
+        return true;
+    }
+}
