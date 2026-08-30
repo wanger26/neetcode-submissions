2b880bcf4acd3b786bdc6bf08c5a1ddfdf4ac8ee
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-30 17:19:53 -0400

    Add: word-ladder - submission-5

---
+++ Data Structures & Algorithms/word-ladder/submission-5.java
@@ -0,0 +1,39 @@
+class Solution {
+    // Time: O(n^2*L) where L is the length of the strings
+    // Space: O(n*L)
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (beginWord.equals(endWord)) {
+            return 0;
+        }
+
+        int result = 0;
+        Set<String> seen = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        seen.add(beginWord);
+        while (!queue.isEmpty()) { // O(n)
+            result++;
+            int initalSize = queue.size();
+            for (int i = 0; i < initalSize; i++) {
+                String word = queue.poll();
+                if (word.equals(endWord)) {
+                    return result;
+                }
+
+                for (int j = 0; j < word.length(); j++) {
+                    for (char character = 'a'; character <= 'z'; character++) {
+                        String newWord = word.substring(0, j) + character + word.substring(j + 1);
+                        if (!wordSet.contains(newWord) || seen.contains(newWord) || newWord.equals(word)) {
+                            continue;
+                        }
+                        queue.add(newWord);
+                        seen.add(newWord);
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+}
