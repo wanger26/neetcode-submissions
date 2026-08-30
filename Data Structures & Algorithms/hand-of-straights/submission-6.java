82d5e5097582e1cc2661b8b170ca64800fd850a7
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-30 09:48:42 -0400

    Add: hand-of-straights - submission-6

---
+++ Data Structures & Algorithms/hand-of-straights/submission-6.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+
+        if(hand.length % groupSize != 0) {
+            return false;
+        }
+
+        SortedMap<Integer, Integer> numToFrequency = new TreeMap<>();
+        for(int num : hand) {
+            numToFrequency.put(num, numToFrequency.getOrDefault(num, 0) + 1);
+        }
+
+        int groupSizeLeft = groupSize;
+        for(int num = numToFrequency.firstKey(); !numToFrequency.isEmpty(); num++) {
+            if(!numToFrequency.containsKey(num)) {
+                return false;
+            }
+            groupSizeLeft--;
+
+            numToFrequency.put(num, numToFrequency.get(num) - 1);
+            if(numToFrequency.get(num) == 0) {
+                numToFrequency.remove(num);
+            }
+
+            if(groupSizeLeft == 0) {
+                groupSizeLeft = groupSize;
+                if(!numToFrequency.isEmpty()) {
+                    num = numToFrequency.firstKey() - 1; // Counter increases it on top
+                }
+            }
+        }
+        
+        return true;
+    }
+}
