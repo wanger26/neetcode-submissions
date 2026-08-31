1bc13812034f1e5adac8966bce86876af3c4ce90
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-08-31 08:53:26 -0400

    Add: longest-common-subsequence - submission-11

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-11.java
@@ -0,0 +1,22 @@
+class Solution {
+    // Time: O(m*n)
+    // Space: O(m*n)
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+
+        int[][] dp = new int[n+1][m+1];
+        for(int i=n-1; i >= 0; i--) {
+            for (int j=m-1; j >= 0; j--) {
+                if(text1.charAt(i) == text2.charAt(j)) {
+                    // We can extend it - do not double count so we grab prev where we picked neither
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                } else {
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
