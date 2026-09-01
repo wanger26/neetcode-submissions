dc91f1e9333b19c773c01880077c9efebdee8411
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-01 08:13:06 -0400

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    // Time: O(n)
+    // Space: O(1)
+    public int rob(int[] nums) {
+        if(nums.length == 1) {
+            return nums[0];
+        }
+
+        return Math.max(robRange(nums, 0, nums.length-1), robRange(nums, 1, nums.length));
+    }
+
+    private int robRange(int[] nums, int start, int end) {
+        int robPrevHouse = 0;
+        int notRobPreviousHouse = 0;
+
+        for(int i=start; i < end; i++) {
+            int robCurrentHouse = notRobPreviousHouse + nums[i];
+            notRobPreviousHouse = Math.max(notRobPreviousHouse, robPrevHouse);
+            robPrevHouse = robCurrentHouse;
+        }
+
+        return Math.max(robPrevHouse, notRobPreviousHouse);
+    }
+}
