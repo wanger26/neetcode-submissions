dbbcb8e357f1b376bd61f90c678608348624e6c1
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-01 22:08:01 -0400

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.java
@@ -0,0 +1,41 @@
+class Solution {
+    // Time: O(n^2)
+    // Space: O(1)
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length-1;
+
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue; // If current character is same as prev, no need to check.
+            }
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(List.of(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                } else if(sum < 0) {
+                    // Need to increase --> move left
+                    left++;
+                } else {
+                    // Need to decrease --> move right
+                    right--;
+                }
+            }
+            
+            
+        }
+
+        return result;
+
+    }
+}
