733bc15b2932ea38fc181b9d06c0a487ff216eb7
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-02 18:01:13 -0400

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int maxBananas = 0;
+        for (int bananas : piles) {
+            maxBananas = Math.max(maxBananas, bananas);
+        }
+
+        int left = 1;
+        int right = maxBananas;
+
+        while (left < right) {
+            int eatingRate = left + (right - left) / 2;
+
+            int hoursTaken = 0;
+            for (int bananas : piles) {
+                hoursTaken += (bananas / eatingRate) + (bananas % eatingRate != 0 ? 1 : 0);
+            }
+
+            if (hoursTaken <= h) {
+                // If we have some headroom on time, lets try and eat less
+                right = eatingRate; // eatingRate could still be the best if less does not have solution
+            } else {
+                // If we took too much time, we need to eat faster
+                left = eatingRate+1;
+            }
+        }
+
+        return left;
+    }
+}
