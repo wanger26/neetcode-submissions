d8113a8c438e3454e11da9f7e046b600137e0d9d
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-02 18:46:46 -0400

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> leftBrackets = new Stack<>();
+        Stack<Integer> stars = new Stack<>();
+
+        // (**
+
+        for (int i=0; i < s.length(); i++) {
+            char character = s.charAt(i);
+            if (character == '(') {
+                leftBrackets.push(i);
+            } else if (character == '*') {
+                stars.push(i);
+            }
+            else {
+                if (leftBrackets.isEmpty() && stars.isEmpty()) {
+                    return false;
+                } else if (!leftBrackets.isEmpty()) {
+                    leftBrackets.pop();
+                } else {
+                    stars.pop();
+                }
+            }
+        }
+
+        while(!leftBrackets.isEmpty() && !stars.isEmpty()) {
+            if(leftBrackets.pop() > stars.pop()) {
+                // If * comes before ( than the solution is no good.
+                return false;
+            }
+        }
+
+        return leftBrackets.isEmpty();
+    }
+}
