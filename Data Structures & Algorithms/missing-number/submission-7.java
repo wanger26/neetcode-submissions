11a9d00ca89cb1ae47764aa60ba6ba0451ffdcf0
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-03 18:18:17 -0400

    Add: missing-number - submission-7

---
+++ Data Structures & Algorithms/missing-number/submission-7.java
@@ -0,0 +1,15 @@
+class Solution {
+    // Time: O(n)
+    // Space: O(1)
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n*(n+1)/2;
+
+        int actualSum = 0;
+        for(int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+}
