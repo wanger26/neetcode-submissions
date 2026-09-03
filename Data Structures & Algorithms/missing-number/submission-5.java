2e213a823e31184cc06deb16a39c0b8e4224d25e
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-03 18:12:53 -0400

    Add: missing-number - submission-5

---
+++ Data Structures & Algorithms/missing-number/submission-5.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    // Time: O(n)
+    // Space: O(1)
+    public int missingNumber(int[] nums) {
+        
+        // [1,2,3]
+        // [1,1,2]
+        for(int i=0; i < nums.length; i++) {
+            if(nums[i] != i && i < nums.length) {
+                int pointer = nums[i];
+                while(pointer < nums.length && nums[pointer] != pointer) {
+                    int temp = nums[pointer];
+                    nums[pointer] = pointer;
+                    pointer = temp;
+                }
+            }
+        }
+
+        for(int i=0; i < nums.length; i++) {
+            if(nums[i] != i) {
+                return i;
+            }
+        }
+
+        return nums.length;
+    }
+}
