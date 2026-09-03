a83365100cf82e03ad4f692259aeadc9af7503ef
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-03 17:59:03 -0400

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    // Time: O(n)
+    // Space: O(n)
+    public int missingNumber(int[] nums) {
+        int[] orderedNums = new int[nums.length+1];
+        orderedNums[0] = -1;
+
+        for(int num : nums) {
+            orderedNums[num] = num;
+        }
+
+        for(int i=0; i < orderedNums.length; i++) {
+            if(orderedNums[i] != i) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
