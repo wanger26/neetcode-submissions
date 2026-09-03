87142d4b0d3ced5793a668051205b63d3d255ad6
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-03 07:23:26 -0400

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    private List<String> result;
+
+    // Time: 
+    public List<String> generateParenthesis(int n) {
+        result = new ArrayList<>();
+        generateParenthesis(n, n, "");
+        return result;
+    }
+
+    private void generateParenthesis(int opening, int closing, String current) {
+        if(opening == 0 && closing == 0) {
+            this.result.add(current);
+        }
+
+        // Option 1: Add another (
+        if(opening > 0) {
+            generateParenthesis(opening-1, closing, current + '(');
+        }
+
+        // Option 2: If opening is less than closing, we can add a closing and keep it valid
+        if(opening < closing) {
+            generateParenthesis(opening, closing-1, current + ')');
+        }
+
+    }
+}
