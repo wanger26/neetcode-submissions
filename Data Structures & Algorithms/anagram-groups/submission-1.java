98d7ad3e453789ff52ea05de522ece9787bcebcb
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-04 17:43:12 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    // Time: O(n*c) where c is the longest word
+    // Space: O(n*c)
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<Map<Character, Integer>, List<String>> charCountToWords = new HashMap<>();
+
+        for(String string : strs) {
+            Map<Character, Integer> charCount = new HashMap<>();
+            for(char character : string.toCharArray()) {
+                charCount.put(character, charCount.getOrDefault(character, 0) + 1);
+            }
+
+            charCountToWords.computeIfAbsent(charCount, k -> new ArrayList<>()).add(string);
+        }
+
+        List<List<String>> result = new ArrayList<>();
+        for(List<String> words : charCountToWords.values()) {
+            result.add(words);
+        }
+
+        return result;
+    }
+}
