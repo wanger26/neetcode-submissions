2a24e1b66862c28e404d7f47529980f2f254247b
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-04 17:50:22 -0400

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+            int pivot = left + (right - left)/2;
+
+            if(nums[pivot] == target) {
+                return pivot;
+            } else if (target < nums[pivot]) {
+                right = pivot - 1;
+            } else {
+                left = pivot + 1; 
+            }
+        }
+
+        return -1;
+    }
+}
