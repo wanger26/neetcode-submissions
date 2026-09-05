71c11aa537dba641673a33817aba27d42eaf9a28
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-05 13:11:49 -0400

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    // Time: O(n)
+    // Space: O(n)
+    public int[] productExceptSelf(int[] nums) {
+        int totalSumAllowingOneZero = 1;
+        int indexOfZero = -1;
+
+        for(int i=0; i < nums.length; i++) {
+            int num = nums[i];
+            if (num == 0) {
+                if(indexOfZero == -1) {
+                    indexOfZero = i;
+                } else {
+                    return new int[nums.length];
+                }
+            } else {
+                totalSumAllowingOneZero *= num;
+            }
+        }
+
+        int[] result = new int[nums.length];
+        if(indexOfZero != -1) {
+            result[indexOfZero] = totalSumAllowingOneZero;
+            return result;
+        }
+
+        for(int i=0; i < nums.length; i++) {
+            result[i] = totalSumAllowingOneZero/nums[i];
+        }
+
+        return result;
+
+    }
+}  
