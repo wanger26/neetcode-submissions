203ae1c95392ee84d3de4436261c9535f1f4560f
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-05 16:20:33 -0400

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        // 2, 2, 4, 6
+        // Pre Sum: 1, 2, 8, 
+        int[] preSum = new int[nums.length];
+        preSum[0] = 1;
+        for(int i=1; i < nums.length; i++) {
+            preSum[i] = preSum[i-1] * nums[i-1];
+        }
+
+        int[] postSum = new int[nums.length];
+        postSum[nums.length-1] = 1;
+        for(int i=nums.length-2; i >= 0; i--) {
+            postSum[i] = postSum[i+1] * nums[i+1];
+        }
+
+        int[] result = new int[nums.length];
+        for(int i=0; i < nums.length; i++) {
+            result[i] = preSum[i] * postSum[i];
+        }
+
+        return result;
+
+    }
+}  
