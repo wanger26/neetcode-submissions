fcf7e2cdb85909980dfaabd5bf4f4e0353da5699
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-05 16:33:28 -0400

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.java
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        intervals.sort((a, b) -> a.start - b.start);
+
+        for(int i=1; i < intervals.size(); i++) {
+            Interval prevInterval = intervals.get(i-1);
+            Interval currentInterval = intervals.get(i);
+
+            if(currentInterval.start < prevInterval.end) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
