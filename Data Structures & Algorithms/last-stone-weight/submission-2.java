42ac9e9bee7dddb119e69baabacb44d63baccd13
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-06 10:16:47 -0400

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    // Time: O(nlogn)
+    // Space: O(n)
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxQueue = new PriorityQueue<>((a,b) -> b-a);
+        for(int stone : stones) {
+            maxQueue.add(stone);
+        }
+
+        while(maxQueue.size() > 1) {
+            int stone1 = maxQueue.poll();
+            int stone2 = maxQueue.poll();
+
+            int result = stone1-stone2;
+            if(result != 0) {
+                maxQueue.add(result);
+            }
+        }
+
+        return maxQueue.isEmpty() ? 0 : maxQueue.poll();
+    }
+}
