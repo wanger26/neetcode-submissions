f841401fd81adbf21fa6acac0e71b2a8a2caa3b3
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-06 10:26:12 -0400

    Add: last-stone-weight - submission-4

---
+++ Data Structures & Algorithms/last-stone-weight/submission-4.java
@@ -0,0 +1,42 @@
+class Solution {
+    // Time: O(n+m) where m is the largest stone
+    // Space: O(m)
+    public int lastStoneWeight(int[] stones) {
+        int maxStone = 0;
+        for(int stone : stones) {
+            maxStone = Math.max(maxStone, stone);
+        }
+
+        int[] buckets = new int[maxStone+1];
+        for(int stone : stones) {
+            buckets[stone]++;
+        }
+
+        int firstStone = maxStone;
+        int secondStone = maxStone;
+
+        while(firstStone > 0) {
+            if(buckets[firstStone] % 2 == 0) {
+                firstStone--;
+                continue;
+            }
+
+            int j = Math.min(firstStone-1, secondStone);
+            while(j > 0 && buckets[j] == 0) {
+                j--;
+            }
+
+            if(j == 0) {
+                return firstStone;
+            }
+
+            secondStone = j;
+            buckets[firstStone]--;
+            buckets[secondStone]--;
+            buckets[firstStone-secondStone]++;
+            firstStone = Math.max(firstStone-secondStone, secondStone);
+        }
+
+        return firstStone;
+    }
+}
