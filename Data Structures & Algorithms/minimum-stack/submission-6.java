870cb0423a51a5bc3561f58145553b0fc355fc11
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-06 17:15:33 -0400

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.java
@@ -0,0 +1,48 @@
+class MinStack {
+
+    // Time: O(1)
+    // Space: O(n)
+    private long min;
+    private Stack<Long> stack;
+
+    public MinStack() {
+        this.stack = new Stack<>();   
+        this.min = 0;
+    }
+    
+    public void push(int val) {
+        if(stack.isEmpty()) {
+            stack.push(0L);
+            min = val;
+        } else {
+            stack.push(val - min);
+            if(val < min) {
+                min = val;
+            }
+        }
+    }
+    
+    public void pop() {
+        if(stack.isEmpty()) {
+           return;
+        }
+
+        long value = stack.pop();
+        if(value < 0) {
+            min = min - value;
+        }
+    }
+    
+    public int top() {
+        long top = stack.peek();
+        if(top > 0) {
+            return (int) (top + min);
+        } else {
+            return (int) min;
+        }
+    }
+    
+    public int getMin() {
+        return (int) min;
+    }
+}
