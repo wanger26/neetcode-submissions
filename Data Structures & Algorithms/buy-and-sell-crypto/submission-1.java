ba22bc085d5c8dd962b6b77a8bdf2cffe30c067c
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-05 22:06:58 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    // Time: O(n)
+    // Space: O(1)
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int currentBuyPrice = prices[0];
+        for(int i=1; i < prices.length; i++) {
+            if(prices[i] >= currentBuyPrice) {
+                maxProfit = Math.max(maxProfit, prices[i] - currentBuyPrice);
+            } else {
+                currentBuyPrice = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+}
