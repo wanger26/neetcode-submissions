a61ad6217036cae1c15b3aa95dc74e34836ea539
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-06 17:41:12 -0400

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        Integer[][] memo = new Integer[amount+1][coins.length];
+        return dp(amount, 0, coins, memo);
+    }
+
+    private int dp(int amountLeft, int index, int[] coins, Integer[][] memo) {
+        if(amountLeft == 0) {
+            return 1;
+        } else if (memo[amountLeft][index] != null) {
+            return memo[amountLeft][index];
+        }
+
+        int result = 0;
+        for(int i=index; i < coins.length; i++) {
+            int coin = coins[i];
+            if(coin <= amountLeft) {
+                result += dp(amountLeft - coin, i, coins, memo);
+            }
+        }
+
+        memo[amountLeft][index] = result;
+
+        return result;
+    }
+}
