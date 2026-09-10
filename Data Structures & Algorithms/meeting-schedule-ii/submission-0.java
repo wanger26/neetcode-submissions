bc1e0cd5fd5273187a9a57e9471584e6232cfcd7
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-09 20:40:46 -0400

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.java
@@ -0,0 +1,33 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+
+        if(intervals.isEmpty()) {
+            return 0;
+        }
+
+        intervals.sort((a,b) -> a.start - b.start);
+
+        PriorityQueue<Integer> nextMeetingRoomAvailTime = new PriorityQueue<>((a,b) -> a-b);
+        int result = 0;
+        for(Interval meeting : intervals) {
+            nextMeetingRoomAvailTime.add(meeting.end);
+            if(nextMeetingRoomAvailTime.isEmpty() || meeting.start >= nextMeetingRoomAvailTime.peek()) {
+                nextMeetingRoomAvailTime.poll();
+            }
+            result = Math.max(result, nextMeetingRoomAvailTime.size());
+        }
+
+        return result;
+    }
+}
