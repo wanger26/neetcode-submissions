0d7d1031d968243f0aa7bfa07f69d292ba6129c7
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-11 07:37:51 -0400

    Add: merge-triplets-to-form-target - submission-0

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        boolean[] found = new boolean[target.length];
+
+        for(int i=0; i < triplets.length; i++) {
+            if(triplets[i][0] > target[0] || triplets[i][1] > target[1] || triplets[i][2] > target[2]) {
+                // Can't use since we will overshoot target
+                continue;
+            }
+
+            if(triplets[i][0] == target[0]) {
+                found[0] = true;
+            }
+
+            if(triplets[i][1] == target[1]) {
+                found[1] = true;
+            }
+
+            if(triplets[i][2] == target[2]) {
+                found[2] = true;
+            }
+
+
+            if(found[0] && found[1] && found[2]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
