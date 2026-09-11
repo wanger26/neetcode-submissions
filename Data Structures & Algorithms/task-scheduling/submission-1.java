9b4f5c05e7b771e6185c049d2ec8b803c769cb3d
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-10 20:28:03 -0400

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.java
@@ -0,0 +1,47 @@
+class Solution {
+
+    // Time: O(n)
+    // Space: O(1)
+    public int leastInterval(char[] tasks, int n) {
+        
+        int[] taskCount = new int[26];
+        for(char task : tasks) {
+            taskCount[task - 'A']++;
+        }
+
+
+        PriorityQueue<Integer> taskCountMaxHeap = new PriorityQueue<>((a,b) -> taskCount[b] - taskCount[a]);
+        for(int i = 0; i < 26; i++) {
+            if(taskCount[i] > 0) {
+                taskCountMaxHeap.add(i);
+            }
+        }
+        
+        int[] coolDownTime = new int[26];
+        PriorityQueue<Integer> taskCoolDownMinHeap = new PriorityQueue<>((a,b) -> coolDownTime[a]-coolDownTime[b]);
+
+        int currentTime = 0;
+        while(!taskCountMaxHeap.isEmpty() || !taskCoolDownMinHeap.isEmpty()) {
+            currentTime++;
+            while(!taskCoolDownMinHeap.isEmpty() && coolDownTime[taskCoolDownMinHeap.peek()] < currentTime) {
+                int index = taskCoolDownMinHeap.poll();
+                if(taskCount[index] > 0) {
+                    taskCountMaxHeap.add(index);
+                }
+            }
+
+            if(!taskCountMaxHeap.isEmpty()) {
+                int index = taskCountMaxHeap.poll();
+                taskCount[index]--;
+
+                if(taskCount[index] > 0) {
+                    coolDownTime[index] = currentTime + n;
+                    taskCoolDownMinHeap.add(index);
+                }
+            }
+        }
+
+        return currentTime;
+
+    }
+}
