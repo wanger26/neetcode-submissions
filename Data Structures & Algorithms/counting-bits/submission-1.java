16967758125dc7d4d91cc2efa1f0f71c978e2d3a
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-11 17:53:34 -0400

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    // Time: O(n)
+    // Space: O(1)
+    public int[] countBits(int n) {
+        int[] dp = new int[n+1];
+
+        int offset = 1;
+        for(int i=1; i <= n; i++) {
+            if(offset * 2 == i) {
+                offset = i;
+            }
+            dp[i] = 1 + dp[i-offset];
+        }
+
+        return dp;
+    }
+}
