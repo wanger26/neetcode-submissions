724ae29999c6a02bbcce8e2fbc72db394c168928
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-13 18:05:13 -0400

    Add: longest-increasing-path-in-matrix - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-3.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int longestIncreasingPath(int[][] matrix) {
+        int result = 0;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] memo = new int[n][m];
+
+        for(int x=0; x < n; x++) {
+            for(int y=0; y < m; y++) {
+                result = Math.max(result, dfs(matrix, x, y, Integer.MIN_VALUE, memo));
+            }
+        }
+
+        return result;
+    }
+
+    private int dfs(int[][] matrix, int x, int y, int prevValue, int[][] memo) {
+        if(x < 0 || x == matrix.length || y < 0 || y == matrix[0].length || matrix[x][y] <= prevValue) {
+            return 0;
+        } else if (memo[x][y] != 0) {
+            return memo[x][y];
+        }
+
+        int result = dfs(matrix, x-1, y, matrix[x][y], memo);
+        result = Math.max(result, dfs(matrix, x+1, y, matrix[x][y], memo));
+        result = Math.max(result, dfs(matrix, x, y-1, matrix[x][y], memo));
+        result = Math.max(result, dfs(matrix, x, y+1, matrix[x][y], memo));
+
+        memo[x][y] = result+1;
+        return memo[x][y];
+    }
+}
