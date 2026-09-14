80d84ade89538b8db6f5efc27255a770da33e970
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-14 19:50:53 -0400

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.java
@@ -0,0 +1,55 @@
+class Solution {
+
+    // Time: O(n)
+    public String encode(List<String> strs) {
+
+        if(strs == null) {
+            return null;
+        }
+
+        StringBuilder bldr = new StringBuilder();
+        for(String str : strs) {
+            bldr.append(str.length()).append("#");
+        }
+
+        if(strs.isEmpty()) {
+            bldr.append("#");        
+        }
+        bldr.append("#");
+
+        for(String str : strs) {
+            bldr.append(str);
+        }
+
+        return bldr.toString();
+    }
+
+    // Time: O(n)
+    public List<String> decode(String str) {
+        System.out.println(str);
+        if(str == null) {
+            return null;
+        }
+
+        int sizeIndex = 0;
+        int sizeIndexEnd = 0;
+        while(str.charAt(sizeIndexEnd) != '#' || str.charAt(sizeIndexEnd+1) != '#') {
+            sizeIndexEnd++;
+        }
+
+        int wordIndex = sizeIndexEnd+2;
+        List<String> result = new ArrayList<>();
+        while(sizeIndex < sizeIndexEnd) {
+            int hashIndex = str.indexOf('#', sizeIndex);
+
+            int length = Integer.parseInt(str.substring(sizeIndex, hashIndex));
+            result.add(str.substring(wordIndex, wordIndex + length));
+
+            sizeIndex = hashIndex+1;
+            wordIndex += length;
+        }
+
+        return result;
+
+    }
+}
