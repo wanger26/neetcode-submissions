bc07bec1585363d3a731d8e1df53ea26e63c190f
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-16 07:58:32 -0400

    Add: set-zeroes-in-matrix - submission-1

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        // Time: O(m*n)
+        // Time: O(z) where z is the number of zeros 
+        Set<Integer> colWithZero = new HashSet<>();
+        Set<Integer> rowsWithZero = new HashSet<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    colWithZero.add(i);
+                    rowsWithZero.add(j);
+                }
+            }
+        }
+
+        for (int i : colWithZero) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+
+        for (int j : rowsWithZero) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
