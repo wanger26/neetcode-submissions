ad5207a2e21df65e952170737ca31ceb7264c299
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-16 08:11:01 -0400

    Add: set-zeroes-in-matrix - submission-3

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-3.java
@@ -0,0 +1,53 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        // Time: O(m*n)
+        // Time: O(1)
+
+        boolean firstRowMustBeZeroed = false;
+        boolean firstColMustBeZeroed = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+
+                    if(i == 0) {
+                        firstRowMustBeZeroed = true;
+                    }
+
+                    if(j == 0) {
+                        firstColMustBeZeroed = true;
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int j = 1; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 0; i < matrix.length; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(firstRowMustBeZeroed) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(firstColMustBeZeroed) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
