576303f498435d52481e68743498bf212d1e41bc
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-17 17:38:23 -0400

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        // Time: O(n*2^n)
+        // Space: O(n) 
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+
+        for(int num : nums) {
+            int size = result.size();
+            for(int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(result.get(i));
+                subset.add(num);
+                result.add(subset);
+            }
+        }
+
+        return result;
+    }
+}
