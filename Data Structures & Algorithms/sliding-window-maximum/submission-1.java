c8bbdab40e1534af1993f7fe1f32bb387bc0d6ca
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-17 18:57:14 -0400

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        // Time: O(nlogk)
+        // Space: O(k)
+        int[] result = new int[nums.length + 1 - k];
+
+        TreeMap<Integer, Integer> sortedMap = new TreeMap<>();
+
+        for(int i = 0; i < k && i < nums.length; i++) {
+            sortedMap.put(nums[i], sortedMap.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int left = 0;
+        int right = k;
+
+        while(right < nums.length) {
+            result[left] = sortedMap.lastKey();
+            sortedMap.put(nums[left], sortedMap.get(nums[left]) -1);
+            if(sortedMap.get(nums[left]) == 0) {
+                sortedMap.remove(nums[left]);
+            }
+
+            sortedMap.put(nums[right], sortedMap.getOrDefault(nums[right], 0) + 1);
+            left++;
+            right++;
+        }
+        result[left] = sortedMap.lastKey();
+
+        return result;
+    }
+}
