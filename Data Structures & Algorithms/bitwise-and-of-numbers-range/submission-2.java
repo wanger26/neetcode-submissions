6665a56109980bfa43f8ef3d3d3a99c3edb557b8
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-18 08:27:19 -0400

    Add: bitwise-and-of-numbers-range - submission-2

---
+++ Data Structures & Algorithms/bitwise-and-of-numbers-range/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        
+        int prevResult = right;
+        int result = right;
+        right--;
+        while(right >= left && prevResult >= result) {
+            prevResult = result;
+            result &= right;
+            right--;
+        }
+
+        return result;
+    }
+}
