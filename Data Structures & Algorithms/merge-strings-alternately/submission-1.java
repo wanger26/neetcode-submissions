8f031e5ae0fdbd8e0fc52f7059cabf98889ff5ef
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-18 08:13:44 -0400

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public String mergeAlternately(String word1, String word2) {
+        // Time: O(n+m)
+        // Space: O(1)
+        StringBuilder bldr = new StringBuilder();
+
+        int index = 0;
+
+        while(index < word1.length() && index < word2.length()) {
+            bldr.append(word1.charAt(index));
+            bldr.append(word2.charAt(index));
+            index++;
+        }
+
+        if(index < word1.length()) {
+            bldr.append(word1.substring(index));
+        } else if (index < word2.length()) {
+            bldr.append(word2.substring(index));
+        }
+
+        return bldr.toString();
+    }
+}
