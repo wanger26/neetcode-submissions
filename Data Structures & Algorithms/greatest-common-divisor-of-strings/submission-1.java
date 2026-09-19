6fa762ce005962e18a92231b6d8d68432fd49ba2
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-19 10:53:41 -0400

    Add: greatest-common-divisor-of-strings - submission-1

---
+++ Data Structures & Algorithms/greatest-common-divisor-of-strings/submission-1.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        
+        int length1 = str1.length();
+        int length2 = str2.length();
+        
+        if (length1 < length2) {
+            return gcdOfStrings(str2, str1);
+        }
+
+        for(int i=length2; i >= 1; i--) {
+            if(length1 % i != 0 || length2 % i != 0) {
+                continue;
+            }
+
+            String candidate = str2.substring(0, i);
+            StringBuilder bldr1 = new StringBuilder();
+            for(int j = 0; j < length1/i; j++) {
+                bldr1.append(candidate);
+            }
+
+            StringBuilder bldr2 = new StringBuilder();
+            for(int j = 0; j < length2/i; j++) {
+                bldr2.append(candidate);
+            }
+
+            if(bldr1.toString().equals(str1) && bldr2.toString().equals(str2)) {
+                return candidate;
+            }
+        }
+
+        return "";
+        
+    }
+}
