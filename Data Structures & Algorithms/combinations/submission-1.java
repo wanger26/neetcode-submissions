55db4bdf01a5d4e8cbbccaf16e6f27484f12ac56
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-19 13:04:35 -0400

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        // Time: O(n!/(k! (n-k)!))
+        // Space: O(k)
+        List<List<Integer>> result = new ArrayList<>();
+        recursive(1, n, k, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void recursive(int number, int n, int k, List<Integer> currentPath, List<List<Integer>> result) {
+        if (currentPath.size() == k) {
+            result.add(new ArrayList<>(currentPath));
+            return;
+        }
+
+        // O(n)
+        for (int i = number; i <= n; i++) {
+            currentPath.add(i);
+            recursive(i + 1, n, k, currentPath, result);
+            currentPath.removeLast();
+        }
+    }
+}
