7d4a72a03004898442146ac0b152cb09c6d50b13
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-20 18:54:40 -0400

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        // Time: O(n)
+        // Space: O(1)
+        int result = nums[0];
+        int count = 1;
+
+        for(int i=1; i < nums.length; i++) {
+            int num = nums[i];
+            if (result == num) {
+                count++;
+            } else {
+                count--;
+            }
+
+            if(count < 0) {
+                result = num;
+                count = 1;
+            }
+        }
+
+        return result;
+    }
+}
