717579ea913802d714adb2031d3656b32130d0d8
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-20 09:49:10 -0400

    Add: jump-game-vii - submission-5

---
+++ Data Structures & Algorithms/jump-game-vii/submission-5.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canReach(String s, int minJump, int maxJump) {
+        // Time: O(n)
+        // Space: O(n)
+        int n = s.length();
+
+        if(s.charAt(n-1) == '1') {
+            return false;
+        }
+
+        boolean[] dp = new boolean[n];
+        dp[n-1] = true;
+
+        for(int i = n-1; i >= 0; i--) {
+            if(s.charAt(i) == '0') {
+                for(int jump = Math.min(maxJump, n-1-i); jump >= minJump && dp[i] == false; jump--) {
+                    dp[i] = dp[i + jump];
+                }
+            }
+        }
+
+        return dp[0];
+    }
+}
