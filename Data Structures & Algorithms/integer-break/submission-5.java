285f7a0cbf40b0219165ec31aa9b2984e62d5cc1
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-21 18:53:23 -0400

    Add: integer-break - submission-5

---
+++ Data Structures & Algorithms/integer-break/submission-5.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int integerBreak(int n) {
+        // Time: O(n^2)
+        // Space: O(n)
+        return dfs(n, n, new Integer[n+1]);
+    }
+
+    private int dfs(int originalNumber, int currentNumber, Integer[] memo) {
+        if (currentNumber == 1) {
+            return 1;
+        } else if (memo[currentNumber] != null) {
+            return memo[currentNumber];
+        }
+
+        // Ensure we split the original number at least once
+        int result = originalNumber == currentNumber ? 0 : currentNumber;
+
+        for(int leftValue = 1; leftValue < currentNumber; leftValue++) {
+            int value = dfs(originalNumber, leftValue, memo) * dfs(originalNumber, currentNumber - leftValue, memo);
+            result = Math.max(result, value);
+        }
+
+        memo[currentNumber] = result;
+        return result;
+    }
+}
