bce098f54ae09cda2e3e7a6db5095f3e4ac09c29
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-22 07:26:02 -0400

    Add: permutations-ii - submission-1

---
+++ Data Structures & Algorithms/permutations-ii/submission-1.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+        // Time: O(n*n!)
+        // Space: O(n)
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtrack(nums, new HashSet<>(), new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void backtrack(int[] nums, Set<Integer> indexUsed, List<Integer> currentPath, List<List<Integer>> result) {
+        if(currentPath.size() == nums.length) {
+            result.add(new ArrayList<>(currentPath));
+            return;
+        }
+
+        for(int i=0; i < nums.length; i++) {
+
+            if(indexUsed.contains(i)) {
+                continue;
+            }
+
+            indexUsed.add(i);
+            currentPath.add(nums[i]);
+            backtrack(nums, indexUsed, currentPath, result);
+            indexUsed.remove(i);
+            currentPath.removeLast();
+
+            // Skip duplicates ahead to avoid starting same seqeunce
+            while(i+1 < nums.length && nums[i] == nums[i+1]) {
+                i++;
+            }
+        }
+    }
+}
