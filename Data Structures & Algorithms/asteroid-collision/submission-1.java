ece6897fa1672522972d4ad8921b5962df25142e
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-22 19:16:19 -0400

    Add: asteroid-collision - submission-1

---
+++ Data Structures & Algorithms/asteroid-collision/submission-1.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> asteroidsMovingRight = new ArrayDeque<>();
+        List<Integer> asteroidsMovedLeftSuccesfully = new ArrayList<>();
+
+        for (int i=0; i < asteroids.length; i++) {
+            if (asteroids[i] < 0) {
+                while (!asteroidsMovingRight.isEmpty() && asteroidsMovingRight.peekLast() < Math.abs(asteroids[i])) {
+                    asteroidsMovingRight.removeLast();
+                }
+
+                if(!asteroidsMovingRight.isEmpty() && asteroidsMovingRight.peekLast() == Math.abs(asteroids[i])) {
+                    asteroidsMovingRight.removeLast();
+                    continue;
+                }
+
+                if(asteroidsMovingRight.isEmpty()) {
+                    asteroidsMovedLeftSuccesfully.add(asteroids[i]);
+                }
+
+            } else {
+                asteroidsMovingRight.addLast(asteroids[i]);
+            }
+        }
+
+        int[] result = new int[asteroidsMovedLeftSuccesfully.size() + asteroidsMovingRight.size()];
+        for (int i = 0; i < result.length; i++) {
+            if(i < asteroidsMovedLeftSuccesfully.size()) {
+                result[i] = asteroidsMovedLeftSuccesfully.get(i);
+            } else {
+                result[i] = asteroidsMovingRight.removeFirst();
+            }
+        }
+
+        return result;
+    }
+}
