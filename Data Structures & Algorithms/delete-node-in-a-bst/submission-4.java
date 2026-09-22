547cdcebc8b52c87621a03ea03f51e20ef4d043e
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-22 18:01:06 -0400

    Add: delete-node-in-a-bst - submission-4

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-4.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        // Time: O(logn)
+        // Space: O(logn)
+        return findAndDelete(root, key);
+    }
+
+    private TreeNode findAndDelete(TreeNode currentNode, int key) {
+        if (currentNode == null) {
+            return null;
+
+        } else if (currentNode.val == key) {
+            if (currentNode.right != null) {
+                TreeNode minNode = currentNode.right;
+                while (minNode.left != null) {
+                    minNode = minNode.left;
+                }
+                currentNode.val = minNode.val;
+                currentNode.right = findAndDelete(currentNode.right, minNode.val);
+                return currentNode;
+            } else if (currentNode.left != null) {
+                return currentNode.left;
+            } else {
+                // Removing a leaf
+                return null;
+            }
+        }
+
+        if (key < currentNode.val) {
+            currentNode.left = findAndDelete(currentNode.left, key);
+        } else {
+            currentNode.right = findAndDelete(currentNode.right, key);
+        }
+
+        return currentNode;
+    }
+}
