4adde6e2a33df6876f064238a7162482dda36636
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-23 18:51:08 -0400

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        // Time: O(n*k)
+        // Space: O(1)
+
+        for(int i=0; i < nums.length; i++) {
+            for(int j = i + 1; j - i <= k && j < nums.length; j++) {
+                if(nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
