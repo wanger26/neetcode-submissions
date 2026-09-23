270e0313139dba27f232ab9525378258740b6e4a
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-23 08:43:47 -0400

    Add: lemonade-change - submission-1

---
+++ Data Structures & Algorithms/lemonade-change/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+
+        // Time: O(n)
+        // Space: O(1)
+        int fives = 0;
+        int tens = 0;
+
+        for(int bill : bills) {
+            if(bill == 5) {
+                fives++;
+            } else if (bill == 10) {
+                if(fives == 0) {
+                    return false;
+                }
+                fives--;
+                tens++;
+            } else {
+                int changeNeeded = 15;
+                if(tens > 0) {
+                    changeNeeded -= 10;
+                    tens--;
+                }
+
+                if(changeNeeded/5 > fives) {
+                    return false;
+                }
+                fives -= changeNeeded/5;
+            }
+        }
+
+        return true;
+    }
+}
