b6bf5ba9042dd31fc7f0921ea4b67894e9d95bbe
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-23 18:55:46 -0400

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        // Time: O(n)
+        // Space: O(Min(n+k))
+        int left = 0;
+        int right = k;
+        Set<Integer> slidingWindowValues = new HashSet<>();
+        for(int i = 0; i < k && i < nums.length; i++) {
+            if(!slidingWindowValues.add(nums[i])) {
+                return true;
+            }
+        }
+
+        while (right < nums.length) {
+            if(!slidingWindowValues.add(nums[right])) {
+                return true;
+            }
+
+            slidingWindowValues.remove(nums[left]);
+            left++;
+            right++;
+        }
+        
+        return false;
+    }
+}
