b1cc667505db08da41282302c677bffda868d0d8
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-24 18:58:14 -0400

    Add: perfect-squares - submission-8

---
+++ Data Structures & Algorithms/perfect-squares/submission-8.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numSquares(int n) {
+
+        // Time: O(n*n^0.5)
+        // Space: O(n)
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, n);
+        dp[0] = 0;
+
+        for(int target = 1; target <= n; target++) {
+            for(int base = 1; base * base <= target; base++) {
+                int pefectSquare = base * base;
+                dp[target] = Math.min(dp[target], 1 + dp[target-pefectSquare]);
+            }
+        }
+
+        return dp[n];
+    }
+}
