dc48edb8d0e2d218baff6f5f826ff84cbafb9b3f
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-24 18:52:43 -0400

    Add: perfect-squares - submission-6

---
+++ Data Structures & Algorithms/perfect-squares/submission-6.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numSquares(int n) {
+
+        // Time: O(n * n^1/2)
+        // Space: O(n)
+        boolean[] visited = new boolean[n+1];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(n);
+
+        int result = 0;
+        while(!queue.isEmpty()) {
+            int initalSize = queue.size();
+            result++;
+            for(int k = 0; k < initalSize; k++) {
+                int current = queue.poll();
+
+                for(int base = 1; base*base <= current; base++) {
+                    int perfectSquare = base*base;
+                    int nextNum = current - perfectSquare;
+
+                    if(nextNum == 0) {
+                        return result;
+                    }
+
+                    if(!visited[nextNum]) {
+                        queue.add(nextNum);
+                        visited[nextNum] = true;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
