5265d5cd8ea198086cf04e5214162a2a18754453
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-24 08:44:28 -0400

    Add: transpose-matrix - submission-1

---
+++ Data Structures & Algorithms/transpose-matrix/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+
+        // Time: O(n*m)
+        // Space: O(1)
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[][] result = new int[m][n];
+
+        for(int i=0; i < n; i++) {
+            for (int j=0; j < m; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+}
