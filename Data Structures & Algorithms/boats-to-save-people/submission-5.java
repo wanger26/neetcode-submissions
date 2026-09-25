5455e69ebd1a3e7318c2f999606168867879f20e
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-25 08:25:47 -0400

    Add: boats-to-save-people - submission-5

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-5.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int max = 0;
+        for (int person : people) {
+            max = Math.max(max, person);
+        }
+
+        int[] count = new int[max + 1];
+        for (int person : people) {
+            count[person]++;
+        }
+
+        int peopleIndex = 0;
+        int countIndex = 1;
+        while (peopleIndex < people.length) {
+            while (count[countIndex] == 0) {
+                countIndex++;
+            }
+
+            people[peopleIndex] = countIndex;
+            count[countIndex]--;
+
+            peopleIndex++;
+        }
+
+        int left = 0;
+        int right = people.length - 1;
+
+        int result = 0;
+        while (left <= right) {
+            // If the lightest person and heaviest person fit, they share the boat
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+
+            // The heaviest person always gets on a boat, either shared or alone
+            right--;
+            result++;
+        }
+
+        return result;
+    }
+}
