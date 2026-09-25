c0931145cfa097a86450dff9067dbf1c60cc71a7
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-24 20:18:54 -0400

    Add: roman-to-integer - submission-4

---
+++ Data Structures & Algorithms/roman-to-integer/submission-4.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int romanToInt(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char character = s.charAt(i);
+
+            int number = switch (character) {
+                case 'I' -> 1;
+                case 'V' -> 5;
+                case 'X' -> 10;
+                case 'L' -> 50;
+                case 'C' -> 100;
+                case 'D' -> 500;
+                default -> 1000;
+            };
+            
+            if (!stack.isEmpty() && stack.peek() < number) {
+                result += number - stack.pop();
+            }
+            else {
+                stack.push(number);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        return result;
+    }
+}
