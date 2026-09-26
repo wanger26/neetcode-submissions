eda3c6a239e975f00b63274a4f299581b37d5806
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-26 10:12:32 -0400

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+            int pivot = left + (right - left)/2;
+            
+            if(target == nums[pivot]) {
+                return pivot;
+            } else if (target < nums[pivot]) {
+                right = pivot - 1;
+            } else {
+                left = pivot + 1;
+            }
+        }
+
+        return left;
+    }
+}
