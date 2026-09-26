69d53a0a9ea2e19b95d43c7aa3be117b9e79ceba
Jakob Wanger <42472879+wanger26@users.noreply.github.com>
2026-09-26 09:57:30 -0400

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String simplifyPath(String path) {
+        
+        Stack<String> stack = new Stack<>();
+        String[] paths = path.split("/");
+
+        for(String current : paths) {
+            if(current.equals("..")) {
+                if(!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if(!current.equals("") && !current.equals(".")) {
+                stack.push(current);
+            }
+        }
+
+        return "/" + String.join("/", stack);
+    }
+}
